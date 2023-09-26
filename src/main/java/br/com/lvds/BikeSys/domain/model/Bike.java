7a6f4ea06aaec252c97f17be64fb7d24ed2afab7
DanielSteil <dani.steil@hotmail.com>
2023-09-25 23:15:55 -0300

    using joinColumn

--- src/main/java/br/com/lvds/BikeSys/domain/model/Bike.java
+++ src/main/java/br/com/lvds/BikeSys/domain/model/Bike.java
@@ -7,9 +7,12 @@ import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -31,6 +34,10 @@ public class Bike implements Serializable {
     @SequenceGenerator(name = "bike_sequence", sequenceName = "bike_seq", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "bike_sequence")
     private BigInteger id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cliets_id")
+    private Client client;
 
     @Column(name = "model")
     private String model;
--- src/main/java/br/com/lvds/BikeSys/domain/model/Client.java
+++ src/main/java/br/com/lvds/BikeSys/domain/model/Client.java
@@ -9,9 +9,12 @@ import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.SequenceGenerator;
@@ -40,7 +43,7 @@ public class Client implements Serializable {
     @Column(name = "number")
     private String number;
 
-    @Column(name = "bikes", columnDefinition = "jsonb[]")
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "clients")
     private List<Bike> bikes;
 
     @Column(name = "created_at")
