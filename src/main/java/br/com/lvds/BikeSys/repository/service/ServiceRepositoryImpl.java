206d1cf3ab11440a5113c064e59dd52a5ffd2575
Daniel Steil <dani.steil@hotmail.com>
2023-10-08 17:51:03 -0300

    .

--- src/main/java/br/com/lvds/BikeSys/repository/service/ServiceRepositoryImpl.java
+++ src/main/java/br/com/lvds/BikeSys/repository/service/ServiceRepositoryImpl.java
@@ -3,6 +3,7 @@ package br.com.lvds.BikeSys.repository.service;
 import java.util.List;
 
 import br.com.lvds.BikeSys.domain.dto.LastServiceDTO;
+import br.com.lvds.BikeSys.domain.model.Service;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.Query;
@@ -16,12 +17,11 @@ public class ServiceRepositoryImpl implements ServiceRepositoryCustom {
     public List<LastServiceDTO> buscaUltimosServicos() {
         StringBuilder sql = new StringBuilder();
         sql.append("""
-            SELECT new br.com.lvds.BikeSys.domain.dto.LastServiceDTO(c.name, b.model, s.description, s.serviceDate)
-            FROM Service s
-            INNER JOIN Bike b ON b.id = s.bikeId 
-            INNER JOIN Client c ON b.id = ANY(c.bikesId)
+            SELECT s.*
+            FROM services s
+            LIMIT 10
         """);
-        Query query = em.createNativeQuery(sql.toString(), LastServiceDTO.class);
+        Query query = em.createNativeQuery(sql.toString(), Service.class);
         return query.getResultList();
     }
     
