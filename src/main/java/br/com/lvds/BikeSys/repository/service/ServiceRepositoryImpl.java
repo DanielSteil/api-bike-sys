7b50365e4029edc5bcb638e8a6691765db1bdfac
Daniel Steil <dani.steil@hotmail.com>
2023-10-08 17:30:05 -0300

    ajusting query

--- src/main/java/br/com/lvds/BikeSys/repository/service/ServiceRepositoryImpl.java
+++ src/main/java/br/com/lvds/BikeSys/repository/service/ServiceRepositoryImpl.java
@@ -16,7 +16,7 @@ public class ServiceRepositoryImpl implements ServiceRepositoryCustom {
     public List<LastServiceDTO> buscaUltimosServicos() {
         StringBuilder sql = new StringBuilder();
         sql.append("""
-            SELECT new br.com.lvds.BikeSys.domain.dto.LastServiceDTO(c."name", b.model, s.description, s.serviceDate)
+            SELECT new br.com.lvds.BikeSys.domain.dto.LastServiceDTO(c.name, b.model, s.description, s.serviceDate)
             FROM Service s
             INNER JOIN Bike b ON b.id = s.fk_bike_id 
             INNER JOIN Client c ON b.id = ANY(c.bikesId)
