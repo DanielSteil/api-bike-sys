0a9e7a66628a2750122c018f443134043746c940
Daniel Steil <dani.steil@hotmail.com>
2023-10-08 18:35:57 -0300

    .

--- src/main/java/br/com/lvds/BikeSys/repository/service/ServiceRepositoryImpl.java
+++ src/main/java/br/com/lvds/BikeSys/repository/service/ServiceRepositoryImpl.java
@@ -2,16 +2,11 @@ package br.com.lvds.BikeSys.repository.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
-import br.com.lvds.BikeSys.domain.dto.LastServiceDTO;
 import br.com.lvds.BikeSys.domain.dto.ServiceDTO;
 import br.com.lvds.BikeSys.domain.mapper.ServiceMapper;
 import br.com.lvds.BikeSys.domain.model.Bike;
 import br.com.lvds.BikeSys.domain.model.Client;
 import br.com.lvds.BikeSys.domain.model.Service;
-import br.com.lvds.BikeSys.repository.BikeRepository;
-import br.com.lvds.BikeSys.repository.client.ClientRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.Query;
@@ -21,17 +16,13 @@ public class ServiceRepositoryImpl implements ServiceRepositoryCustom {
     @PersistenceContext
     EntityManager em;
 
-    @Autowired
-    private BikeRepository bikeRepository;
-
-    private ClientRepository clientRepository;
-
     @Override
     public List<ServiceDTO> buscaUltimosServicos() {
         StringBuilder sql = new StringBuilder();
         sql.append("""
             SELECT s.*
             FROM services s
+            ORDER BY s.service_date DESC
             LIMIT 10
         """);
         Query query = em.createNativeQuery(sql.toString(), Service.class);
