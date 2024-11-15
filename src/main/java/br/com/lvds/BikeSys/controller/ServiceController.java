1e9a6aca2394f6f931976ed11e386a92081a61a4
DANIEL CHIARELLI STEIL <daniel_steil@estudante.sc.senai.br>
2024-11-14 21:00:39 -0300

    .

--- src/main/java/br/com/lvds/BikeSys/controller/ServiceController.java
+++ src/main/java/br/com/lvds/BikeSys/controller/ServiceController.java
@@ -3,9 +3,12 @@ package br.com.lvds.BikeSys.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.lvds.BikeSys.domain.dto.ServiceDTO;
 import br.com.lvds.BikeSys.service.ServiceService;
 import jakarta.validation.Valid;
 
@@ -15,6 +18,11 @@ public class ServiceController {
     
     @Autowired
     private ServiceService serviceService;
+
+    @PostMapping()
+    public ResponseEntity<?> saveService(@RequestBody ServiceDTO serivice) throws Exception {
+        return ResponseEntity.ok(serviceService.createService(serivice));
+    }
 
     @GetMapping("/lasts")
     public ResponseEntity<?> getLastServices(@Valid Long limit) throws Exception {
